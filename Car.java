8dc55efecf81fd9ecee9404242a3d405f31e8478
Kamil Dobek <camildo7@gmail.com>
2024-10-13 19:10:54 +0200

    Updated Car.java

--- Car.java
+++ Car.java
@@ -1,16 +1,24 @@
 public class Car {
     // Attributes
-    
+    String make;
+    String model;
+    int year;
     // Constructor
-    
+    Car(String x,String y,int z){
+        make=x;
+        model=y;
+        year=z;
+    }
     // Method to display car information
-    
+    void displayInfo(){
+        System.out.println(make+" "+model+" "+year);
     }
 
-    public static void main(String[] args) {
-        // Create a Car object
-        
-        // Display car details
-        
-    }
+
+public static void main(String[] args) {
+    // Create a Car object
+    Car car1 = new Car("Toyota","Prius",2015);
+    // Display car details
+    car1.displayInfo();
+}
 }
