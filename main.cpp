6df7592ad3e9a00cae1dc70ae74d9986611050a0
Kamil Dobek <camildo7@gmail.com>
2024-10-13 19:31:45 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
-// include the necessary libraries
+#include <iostream>
+#include <string>
 
-class Car {
+using namespace std;
+
+class Car{
     //attributes
-
+    string make;
+    string model;
+    int year;
     // Constructor
-
+public:
+    Car(string x, string y, int z){
+        make = x;
+        model = y;
+        year = z;
+    }
     // Method to display car information
-   
+    void displayInfo() {
+        cout << make << " " << model << " " << year << endl;
+    };
 };
 
 int main() {
     // Create a Car object
-    
+    Car car1("Toyota","Prius",2015);
     // Display car details
-    
+    car1.displayInfo();
     return 0;
 }
